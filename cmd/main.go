a7afcf96cad12a68d52c5fbc2ba2f3e604c882ac
tutaf <andrei.cernisov@isa.utm.md>
2024-09-24 12:01:54 +0300

    [UPDATE] CORS configuration

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"app/config"
 	"app/database"
 	"app/router"
 	"log"
 
-	fiber "github.com/gofiber/fiber/v2"
-	cors "github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -17,11 +18,21 @@ func main() {
 		ServerHeader:  "Fiber",
 		AppName:       "App Name",
 	})
-	app.Use(cors.New())
 
+	// CORS configuration
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     config.Config("FRONTEND_URL"), // Frontend URL
+		AllowCredentials: true,                          // Allow credentials (cookies)
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS", // Allowed HTTP methods
+		AllowHeaders:     "Content-Type, Authorization", // Allowed headers
+	}))
+
+	// Connect to the database
 	database.ConnectDB()
 
+	// Set up routes
 	router.SetupRoutes(app)
 
+	// Start the server on port 3000
 	log.Fatal(app.Listen(":3000"))
 }
